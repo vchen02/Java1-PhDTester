9d40d0304e49e95335006a3d9a5c1a2c5f8673bd
vchen02@gmail.com <vchen02@gmail.com>
2015-12-29 16:18:04 -0800

    Add PhD class and create JUnit class PhDTester. Completed Group A and Group B per assignment pdf.

--- src/PhD.java
+++ src/PhD.java
@@ -1,4 +1,96 @@
+/** 	NetId: Victor, Chen 
+ * Time spent: hh hours, mm minutes.
+ * An instance maintains info about the PhD of a person. 
+ */
 
+/** An instance of a person with an PhD */
 public class PhD {
 
+	private String name;	//name of the person with a PhD, a length of name > 0
+	private int year;		//year PhD was awarded
+	private int month;		//month the PhD was awarded. In range 1..12, with 1 meaning January, etc
+	private char gender;	//'M' means male and 'F' means female 
+	private PhD firstAdvisor;//The first PhD advisor of this person —null if unknown
+	private PhD secondAdvisor;/*The second advisor of this person
+							  -null if unknown or if the person had only one advisor
+							  -if the first-advisor field is null, 
+							  the second advisor field must be null */
+	private int numOfAdvisees; //number of PhD advisees of this person
+	
+	/** Constructor: an instance for a person with name n, gender g, PhD year y, 
+	 * and PhD month m. Its advisors are unknown, and it has no advisees.
+	 * Precondition: n has at least 1 character, m is in 1..12, and g is 'M' for male 
+	 * or 'F' for female.
+	 */
+	PhD(String n, char g, int y, int m) {
+		
+		//Check Preconditions
+		assert n.length() > 0;
+		assert g == 'M' || g == 'F';
+		assert m > 0 || m < 13;
+		
+		//Initialize instance variables
+		name = n;
+		year = y;
+		month = m;
+		gender = g;
+		firstAdvisor = null;
+		secondAdvisor = null;
+		numOfAdvisees = 0;
+	}
+
+	/** Return this person's name. */
+	public String getName() {
+		return name;
+	}
+	
+	/** Return the year this person got their PhD */
+	public int getYear() {
+		return year;
+	}
+	
+	/** Return the month this person got their PhD */
+	public int getMonth() {
+		return month;
+	}
+	
+	/** Return the value of the sentence "this person is a male.” */
+	public boolean isMale() {
+		return (gender == 'M');
+	}
+	
+	/** Return this PhD's first advisor, null if unknown */
+	public PhD getFirstAdvisor() {
+		return firstAdvisor;
+	}
+	
+	/** Return this PhD's second advisor, null if unknown or nonexistent */
+	public PhD getSecondAdvisor() {
+		return secondAdvisor;
+	}
+	
+	/** Return the number of PhD advisees of this person */
+	public int numAdvisees() {
+		return numOfAdvisees;
+	}
+	
+	/** Add p as this person's first PhD advisor.
+	 *  Precondition: this person's first advisor is unknown and p is not null.
+	 */
+	public void addFirstAdvisor(PhD p) {
+		assert p != null && firstAdvisor == null;
+		firstAdvisor = p;
+		p.numOfAdvisees++;
+	}
+	
+	/** Add p as this persons second advisor.
+	 *  Precondition: This person's first advisor is known, their second 
+	 *  advisor is unknown, p is not null, and p is different from this person's first advisor.
+	 */
+	public void addSecondAdvisor(PhD p) {
+		assert p != null && p != firstAdvisor && secondAdvisor == null && firstAdvisor != null ;
+		secondAdvisor = p;
+		p.numOfAdvisees++;
+	}	
+	
 }
--- src/PhDTester.java
+++ src/PhDTester.java
@@ -1,4 +1,50 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class PhDTester {
-
+	
+	@Test
+	public void testConstructorA() {
+		//Test male PhD constructor
+		PhD male_grad = new PhD("Jacob", 'M', 1989, 6);
+		assertEquals(male_grad.getName(), "Jacob");
+		assertEquals(male_grad.getYear(), 1989);
+		assertEquals(male_grad.getMonth(), 6);
+		assertEquals(male_grad.isMale(), true);
+		assertEquals(male_grad.getFirstAdvisor(), null);
+		assertEquals(male_grad.getSecondAdvisor(), null);
+		assertEquals(male_grad.numAdvisees(), 0);
+		
+		//Test female PhD constructor
+		PhD female_grad = new PhD("Betty", 'F', 1988, 12);
+		assertEquals(female_grad.getName(), "Betty");
+		assertEquals(female_grad.getYear(), 1988);
+		assertEquals(female_grad.getMonth(), 12);
+		assertEquals(female_grad.isMale(), false);
+		assertEquals(female_grad.getFirstAdvisor(), null);
+		assertEquals(female_grad.getSecondAdvisor(), null);
+		assertEquals(female_grad.numAdvisees(), 0);
+	}
+	
+	@Test
+	public void testAdvisorSetterMethodsB() {
+		//create 6 PhDs
+		PhD grad1 = new PhD("Jacob", 'M', 1989, 6);
+		PhD grad2 = new PhD("Bentsy", 'F', 1985, 1);
+		PhD grad3 = new PhD("Annie", 'F', 1992, 7);
+		PhD grad4 = new PhD("Frank", 'M', 1990, 10);
+		PhD grad5 = new PhD("Chris", 'M', 1987, 11);
+		PhD grad6 = new PhD("Noah", 'M', 1982, 3);
+		
+		grad1.addFirstAdvisor(grad2);
+		grad1.addSecondAdvisor(grad3);
+		assertEquals(grad1.getFirstAdvisor(), grad2);
+		assertEquals(grad1.getSecondAdvisor(), grad3);
+		assertEquals(grad2.numAdvisees(), 1);
+		assertEquals(grad3.numAdvisees(), 1);
+		
+		grad2.addFirstAdvisor(grad3);
+		assertEquals(grad3.numAdvisees(), 2);
+	}	
 }
